fe0aae2ac5abf93a97d0b5496999a70d521b8e50
Nikhil <nsc27@case.edu>
2019-11-16 16:33:23 -0500

    Changes to gsheet_api.py made, now does batch read/write

--- gsheet_api.py
+++ gsheet_api.py
@@ -10,13 +10,33 @@ import sys
 import json
 import gspread
 import time
-from oauth2client.client import SignedJwtAssertionCredentials
+#from oauth2client.client import SignedJwtAssertionCredentials
+from oauth2client.client import AssertionCredentials
+from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient import discovery
+import googleapiclient as gapi
+
+
+SHEET_ID = "190fbfWoewEeNyZaJqAoi0erhY3W_DXTGG1UAQZPcHmM"
+
+column_dict = {1:"A",
+			   2:"B",
+			   3:"C",
+			   4:"D",
+			   5:"E",
+			   6:"F",
+			   7:"G",
+			   8:"H",
+			   9:"I",
+			   10:"J"
+			   }
+
 
 # config credentials
 def configCreds(credPath):
-	json_key = json.load(open(credPath))
+	#json_key = json.load(open(credPath))
 	scopeList = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scopeList)
+	credentials = ServiceAccountCredentials.from_json_keyfile_name(credPath, scopeList)
 	return credentials
 
 # reads sheet, returns list of lists
@@ -28,30 +48,19 @@ def readSheet(creds, sheet, subsheet, columns):
 	subsheeet = subsheet
 	columnss = columns
 	try:
-		goog = gspread.authorize(creds)
-		sheet = goog.open(sheet).worksheet(subsheet)
-		i = 2
-		j = 1
-		flag = (sheet.cell(i,j).value != "")
-		while(flag):
-			try:
-				while(j <= columns):
-					entry.append((sheet.cell(i,j).value).encode("utf-8"))
-					j += 1
-				j = 1
-				list.append(entry)
-				entry = []
-				i += 1
-				flag = (sheet.cell(i,j).value != "")
-			except gspread.exceptions.APIError:
-				print("API Limit\n")
-				time.sleep(110)
-				flag = (sheet.cell(i,j).value != "")
+		# goog = gspread.authorize(creds)
+		# sheet = goog.open(sheet).worksheet(subsheet)
+
+		serv = discovery.build('sheets','v4',credentials=creds)
+
+		request = serv.spreadsheets().values().batchGet(spreadsheetId=SHEET_ID,ranges=[str(subsheeet)+'!A2:'+column_dict[columnss]])
+		response=request.execute()
+
 	except gspread.exceptions.APIError:
 		print("API Limit\n")
 		time.sleep(110)
-		readSheet(credss, sheeet, subsheeet, columnss)
-	return list
+		return readSheet(credss, sheeet, subsheeet, columnss)
+	return response['valueRanges'][0]['values']
 
 # finds last occupied cell
 def findLast(sheet):
@@ -66,27 +75,42 @@ def writeData(creds, sheet, subsheet, data, overwrite=False):
 	subsheeet = subsheet
 	dataa = data
 	overwritee = overwrite
+
+
 	try:
-		goog = gspread.authorize(creds)
-		sheet = goog.open(sheet).worksheet(subsheet)
-		j = 1
-		if(overwrite):
-			i = 2
-		else:
-			i = findLast(sheet)
-		for each in data:
-			for datum in each:
-				try:
-					sheet.update_cell(i, j, datum)
-				except gspread.exceptions.APIError:
-					print("API Limit\n")
-					time.sleep(110)
-					sheet.update_cell(i, j, datum)
-				j+=1
-			j = 1
-			i += 1
+
+		serv = discovery.build('sheets', 'v4', credentials=creds)
+
+		body = {'range':str(subsheeet)+'!A2:I',
+				'values':data,
+				'majorDimension':'ROWS'}
+		print(json.dumps(body))
+		request = serv.spreadsheets().values().append(spreadsheetId=SHEET_ID,
+													  range=str(subsheeet)+'!A2:I',valueInputOption='RAW',
+													  insertDataOption='INSERT_ROWS',
+													  body=body)
+
+		try:
+			response = request.execute()
+			print(response)
+		except gapi.errors.HttpError as err:
+			print(err)
+			return
+
+
 	except gspread.exceptions.APIError:
 		print("API Limit\n")
 		time.sleep(110)
 		writeData(credss, sheeet, subsheeet, dataa, overwritee)
 	return True
+
+# def main():
+# 	creds = configCreds("creds.json")
+# 	print('writing')
+# 	rite = [["Johnny Test",69,420,350,5],["EuroStep",68,421,439,4]]
+# 	print(writeData(creds, "Sinister.ly Market Data","User Manifest",rite))
+# 	#print(readSheet(creds, "Sinister.ly Market Data","Market",9))
+# 	print('done')
+#
+# if __name__ == "__main__":
+# 	main()
