867fc78d5d8bc3252e8ef9373adcf4d122fafa3f
gjbsaiia <gjs64@case.edu>
2019-12-09 19:25:07 -0500

    finalized logging and code

--- crawl.py
+++ crawl.py
@@ -52,6 +52,7 @@ def startCrawl(sesh, end=1000):
     writeToLog('Starting to Crawl!')
     writeToLog("**************************************************\n")
     writeToLog("Page number: "+str(page_num))
+    writeTOLog(str(sesh.driver.current_url))
     writeToLog("**************************************************\n")
     current_page = market_url+"1"
     thread = web.stripThread(sesh.driver, current_page, i)
